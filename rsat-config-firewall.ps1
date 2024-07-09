3e72db5f49e957dc3604eaa097861d8c860bf816
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-09 16:34:12 -0300

    rsat-config-firewall.ps1

---
+++ rsat-config-firewall.ps1
@@ -0,0 +1,39 @@
+#########################################
+## RSAT- Habilita regras no Firewall   ##
+#########################################
+
+$lang = Get-WinUserLanguageList | findstr LanguageTag
+$language = ($lang.Split())[-1]
+$language
+
+#########################################
+## Sistema Operacional em Inglês       ##
+#########################################
+
+if ($language -eq "=en-US") {
+Set-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Event Log Management" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Service Management" -Enabled True
+Set-NetFirewallRule -DisplayGroup "File and Printer Sharing" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Scheduled Tasks Management" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Performance Logs and Alerts" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Volume Management" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Windows Firewall Remote Management" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Desktop" -Enabled True
+}
+
+#########################################
+## Sistema Operacional em Português    ##
+#########################################
+
+if ($language -eq "pt-BR") {
+Set-NetFirewallRule -DisplayGroup "Instrumentação de Gerenciamento do Windows (WMI)" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Gerenciamento Remoto do Log de Eventos" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Gerenciamento Remoto de Serviços" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Compartilhamento de Arquivo e Impressora" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Gerenciamento Remoto de Tarefas Agendadas" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Logs e Alertas de Desempenho" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Gerenciamento de Volumes Remoto" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Gerenciamento Remoto do Windows Defender Firewall" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Área de Trabalho Remota" -Enabled True
+}
