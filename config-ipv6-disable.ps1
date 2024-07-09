dbe2d22a82575451565e700f540184e8cdcb69f9
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-09 18:08:55 -0300

    config-ipv6-disable.ps1

--- config-ipv6-disable.ps1
+++ config-ipv6-disable.ps1
@@ -2,14 +2,19 @@
 ##           Desativa o IPV6           ##
 #########################################
 
-# Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceDescription | Index | InterfaceAlias | IPv4Address | IPv4DefaultGateway | DNSServer" | format-list
+## Lista o status dos adaptadores com IPV6
+Get-NetAdapterBinding -ComponentID ms_tcpip6
+## Verifica se esta ativo o protocolo IPV6
+Get-NetIPv6Protocol | fl RandomizeIdentifiers
+
+## Seleciona o primeiro adaptador de rede ativo
 $interface = Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceAlias"
 $InterfaceAlias = (($interface.Split(":"))[-1]).trim()
 
-Get-NetIPv6Protocol | fl RandomizeIdentifiers
+## Desativa o protocolo IPV6
 Set-NetIPv6Protocol -RandomizeIdentifiers Disabled
-Get-NetIPv6Protocol | fl RandomizeIdentifiers
 
-Get-NetAdapterBinding -ComponentID ms_tcpip6
+## Desativa o componente IPV6 da interface de rede ativa
 Disable-NetAdapterBinding -InterfaceAlias "$InterfaceAlias" -ComponentID ms_tcpip6
-Get-NetAdapterBinding -ComponentID ms_tcpip6
+
+# Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceDescription | Index | InterfaceAlias | IPv4Address | IPv4DefaultGateway | DNSServer" | format-list
