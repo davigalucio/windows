a4fefed05f1e2f7c86d8f52a97524ebdcef82bfa
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-09 17:18:05 -0300

    config-rdp-enable.ps1

---
+++ config-rdp-enable.ps1
@@ -0,0 +1,11 @@
+#################################################
+## Habilita Conexão de Area de Trabalho Remota ##
+#################################################
+
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+
+#################################################
+## Habilitando Regra no Firewall               ##
+#################################################
+
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
