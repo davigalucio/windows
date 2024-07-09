e4dde3a06058d3615b842b622eead28ff2faa15f
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-09 16:25:11 -0300

    hypervserver.ps1

--- hypervserver.ps1
+++ hypervserver.ps1
@@ -133,7 +133,7 @@ Set-NetFirewallRule -DisplayGroup "Área de Trabalho Remota" -Enabled True
 ## Instalação do Windows Admin Center  ##
 #########################################
 
-Invoke-WebRequest -Uri https://aka.ms/WACDownload -OutFile "C:\wac.msi"
+Invoke-WebRequest -Uri https://aka.ms/WACDownload -OutFile "$ENV:HOMEDRIVE\wac.msi"
 New-NetFirewallRule -DisplayName "Allow Windows Admin Center Outbound 443" -Direction Outbound -profile Domain -LocalPort 443 -Protocol TCP -Action Allow
 New-NetFirewallRule -DisplayName "Allow Windows Admin Center Inbound 443" -Direction Inbound -profile Domain -LocalPort 443 -Protocol TCP -Action Allow
 $msiArgs = @("/i", "C:\WAC.msi", "/qn", "/L*v", "log.txt", "SME_PORT=443", "SSL_CERTIFICATE_OPTION=generate")
