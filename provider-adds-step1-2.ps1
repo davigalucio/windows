7a029851d1bd0fa3b7f73ca60e976f285612b4e6
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-09 23:56:11 -0300

    provider-adds-step1-2.ps1

--- provider-adds-step1-2.ps1
+++ provider-adds-step1-2.ps1
@@ -1,6 +1,8 @@
+################################################
+# Altere de acordo com seu Ambinte             #
+################################################
 $hostname = 'SRVADDS001M'
 $pwdhost = 'Adds!1234@5.'
-
 $ip = '192.168.0.250'
 $mask = '24'
 $gateway = '192.168.0.1'
@@ -8,25 +10,31 @@ $dns1 = '192.168.0.250'
 $dns2 = '8.8.8.8'
 $dns3 = '127.0.0.1'
 
-# Ajusta o Fuso Horario
+################################################
+# Seleciona o primeiro adaptador de rede ativo #
+################################################
+$interface = Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceAlias"
+$InterfaceAlias = (($interface.Split(":"))[-1]).trim()
+
+################################################
+# Ajusta o Fuso Horario                        #
+################################################
 tzutil /s "E. South America Standard Time"
 
-# Altera a senha atual
+################################################
+# Altera a senha atual                         #
+################################################
 net user $ENV:username $pwdhost
 
-#########################################
-##           Desativa o IPV6           ##
-#########################################
+################################################
+# Desativa o IPV6                              #
+################################################
 
 ## Lista o status dos adaptadores com IPV6
 Get-NetAdapterBinding -ComponentID ms_tcpip6
 
 ## Verifica se esta ativo o protocolo IPV6
 Get-NetIPv6Protocol | fl RandomizeIdentifiers
-
-## Seleciona o primeiro adaptador de rede ativo
-$interface = Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceAlias"
-$InterfaceAlias = (($interface.Split(":"))[-1]).trim()
 
 ## Desativa o protocolo IPV6
 Set-NetIPv6Protocol -RandomizeIdentifiers Disabled
@@ -37,8 +45,6 @@ Disable-NetAdapterBinding -InterfaceAlias "$InterfaceAlias" -ComponentID ms_tcpi
 #########################################
 ##          Desativa o DHCP            ##
 #########################################
-$interface = Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceAlias"
-$InterfaceAlias = (($interface.Split(":"))[-1]).trim()
 Set-NetIPInterface -InterfaceAlias "$InterfaceAlias" -Dhcp Disabled
 
 #########################################
