80b6d0b06fcbb71f3c28fdf69efd6c8103bd100c
davigalucio <82713318+davigalucio@users.noreply.github.com>
2024-07-10 14:55:30 -0300

    Create provider-adds-step3-3.ps1

---
+++ provider-adds-step3-3.ps1
@@ -0,0 +1,25 @@
+###################################
+# Reconfigura informações de Rede #
+###################################
+$dns1 = '192.168.0.250'
+$dns2 = '8.8.8.8'
+$dns3 = '127.0.0.1'
+
+$interface = Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null} | findstr "InterfaceAlias"
+$InterfaceAlias = (($interface.Split(":"))[-1]).trim()
+Set-DNSClientServerAddress -InterfaceAlias "$InterfaceAlias" -ServerAddresses ("$dns1","$dns2","$dns3")
+Restart-NetAdapter -InterfaceAlias $InterfaceAlias
+
+###################################
+# Adiciona Zona Reversa Primaria  #
+###################################
+$ip1=(Get-NetIPConfiguration | findstr IPv4Address).Split(":,.")[1].trim()
+$ip2=(Get-NetIPConfiguration | findstr IPv4Address).Split(":,.")[2].trim()
+$ip3=(Get-NetIPConfiguration | findstr IPv4Address).Split(":,.")[3].trim()
+$mask=(Get-NetIPAddress -InterfaceAlias "$InterfaceAlias" | findstr PrefixLength).Split(":")[1].trim()
+
+Add-DnsServerPrimaryZone -NetworkID "$ip1.$ip2.$ip3.0/$mask" -ReplicationScope "Forest"
+
+# Fonte:
+# https://eltonbordim.com.br/instalando-o-servico-do-active-directory-domain-services-adds/
+# https://learn.microsoft.com/pt-br/windows-server/networking/dns/manage-dns-zones?tabs=powershell
